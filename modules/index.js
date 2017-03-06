67798d20eec069f8e0bc5a12aeda5be733ad2232
Timi <timiroberts@gmail.com>
2017-03-06 13:16:12 -0500

    Remove component functions

--- modules/index.js
+++ modules/index.js
@@ -1,25 +1,5 @@
 const utils = require('./utils.js')
-const { defaultActions, configureFromTypes } = utils
-const React = require('react');
-
-const configuredWith = ({
-  configuration,
-  Component,
-  actions = defaultActions,
-  ...passedProps
-}) => class extends React.Component {
-  configureData = configureFromTypes(actions)(configuration)
-  render(){
-    return (
-      <Component
-        {...this.configureData(this.props)}
-        {...passedProps}
-      />
-    )
-  }
-}
 
 module.exports = {
   utils,
-  configuredWith,
 }
